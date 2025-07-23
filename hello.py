d394cff670559a0fff6055417c2421530dee330f
dineshsky22 <dineshkumarsatha22@gmail.com>
2025-07-23 16:18:21 +0530

    true

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
-a = 'hello'
-print(b)
+
+n = int(("Enter a number: "))
+
+i = 1
+while :
+    value = 2 ** i
+    if value > n:
+        print(f"The smallest power of 2 greater than {n} is {value}")
+        break
+    i += 1
